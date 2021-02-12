f4c8ea082ea48639652a3a545922ec20f4aec167
Matt <matt@mspacecreative.com>
2021-02-12 09:22:52 -0400

    c

--- templates/blocks/loops/loop-latest-news-article.php
+++ templates/blocks/loops/loop-latest-news-article.php
@@ -15,11 +15,11 @@ $blurb = get_the_excerpt(); ?>
 <div class="col col-lg-4 col-md-4 col-sm-6 col-xs-12">
 	<div class="col_inner">
 		<h2><?php echo __('News'); ?></h2>
-		<h3><?php echo $title ?></h3>
+		<h3 style="margin-bottom: 0;"><?php echo $title ?></h3>
 		<p class="date"><?php echo $date ?></p>
 		<p class="excerpt"><?php echo $blurb ?></p>
 		<p class="inline">
-			<a class="read_more" href="<?php echo the_permalink(); ?>"><?php echo __('Read more'); ?></a>
+			<a class="read_more" href="<?php echo the_permalink(); ?>"><?php echo __('Read post'); ?></a>
 		</p>
 	</div>
 </div>
