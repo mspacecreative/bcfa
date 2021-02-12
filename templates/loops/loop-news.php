e1d5d59fc13ab288f2ebf4184f8300692449a1fa
Matt <matt@mspacecreative.com>
2021-02-12 13:17:31 -0400

    c

--- templates/loops/loop-news.php
+++ templates/loops/loop-news.php
@@ -14,7 +14,7 @@ if ( $loop->have_posts() ) : ?>
 	$title = get_the_title();
 	$date = get_the_date();
 	$blurb = get_the_excerpt();
-	$newsletter = get_the_category($post->ID); ?>
+	$newsletter = is_category('newsletter'); ?>
 	
 	<div class="col col-lg-4 col-md-4 col-sm-6 col-xs-12">
 		<div class="col_inner">
@@ -22,7 +22,7 @@ if ( $loop->have_posts() ) : ?>
 			<p class="date"><?php echo $date ?></p>
 			<p class="excerpt"><?php echo $blurb ?></p>
 			<p class="inline">
-				<?php if ( $newsletter === 'newsletter' ) : ?>
+				<?php if ( $newsletter ) : ?>
 				<a class="read_more" href="<?php echo the_permalink(); ?>"><?php echo __('View newsletter'); ?></a>
 				<?php else : ?>
 				<a class="read_more" href="<?php echo the_permalink(); ?>"><?php echo __('Read post'); ?></a>
