0b93ac87f82f1c46eb22c224f1f58a1e13e3a274
Matt <matt@mspacecreative.com>
2021-02-12 11:21:38 -0400

    c

--- templates/loops/loop-news.php
+++ templates/loops/loop-news.php
@@ -14,7 +14,6 @@ $blurb = get_the_excerpt(); ?>
 
 <div class="col col-lg-4 col-md-4 col-sm-6 col-xs-12">
 	<div class="col_inner">
-		<h2><?php echo __('News'); ?></h2>
 		<h3 style="margin-bottom: 0;"><?php echo $title ?></h3>
 		<p class="date"><?php echo $date ?></p>
 		<p class="excerpt"><?php echo $blurb ?></p>
