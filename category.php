66c260b64b2c3060511636bac550633f8ce9b8aa
Matt <matt@mspacecreative.com>
2021-02-12 13:57:04 -0400

    c

--- category.php
+++ category.php
@@ -2,18 +2,29 @@
 
 	<main role="main" aria-label="Content">
 		<!-- section -->
-		<section>
+		<section id="main-content">
+			
+			<div class="innerContainer">
+				
+				<div class="row sidebar_layout">
+					
+					<div class="col col-lg-8 col-md-8 col-sm-12 col-xs-12 bottomMarginMobile">
 
-			<h1><?php esc_html_e( 'Category: ', 'html5blank' ); single_cat_title(); ?></h1>
-
-			<?php get_template_part( 'loop' ); ?>
-
-			<?php get_template_part( 'pagination' ); ?>
+						<h1 class="page_title"><?php esc_html_e( 'Category: ', 'html5blank' ); single_cat_title(); ?></h1>
+			
+						<?php get_template_part( 'loop' ); ?>
+						<?php get_template_part( 'pagination' ); ?>
+						
+					</div>
+					
+				</div>
+				
+				<?php get_sidebar(); ?>
+				
+			</div>
 
 		</section>
 		<!-- /section -->
 	</main>
-
-<?php get_sidebar(); ?>
 
 <?php get_footer(); ?>
--- loop.php
+++ loop.php
@@ -1,47 +1,50 @@
-<?php if (have_posts()): while (have_posts()) : the_post(); ?>
-
-	<!-- article -->
-	<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-
-		<!-- post thumbnail -->
-		<?php if ( has_post_thumbnail() ) : // Check if thumbnail exists. ?>
-			<a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>">
-				<?php the_post_thumbnail( array( 120, 120 ) ); // Declare pixel size you need inside the array. ?>
-			</a>
-		<?php endif; ?>
-		<!-- /post thumbnail -->
-
-		<!-- post title -->
-		<h2>
-			<a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>"><?php the_title(); ?></a>
-		</h2>
-		<!-- /post title -->
-
-		<!-- post details -->
-		<span class="date">
-			<time datetime="<?php the_time( 'Y-m-d' ); ?> <?php the_time( 'H:i' ); ?>">
-				<?php the_date(); ?> <?php the_time(); ?>
-			</time>
-		</span>
-		<span class="author"><?php esc_html_e( 'Published by', 'html5blank' ); ?> <?php the_author_posts_link(); ?></span>
-		<span class="comments"><?php if ( comments_open( get_the_ID() ) ) comments_popup_link( __( 'Leave your thoughts', 'html5blank' ), __( '1 Comment', 'html5blank' ), __( '% Comments', 'html5blank' ) ); ?></span>
-		<!-- /post details -->
-
-		<?php html5wp_excerpt( 'html5wp_index' ); // Build your custom callback length in functions.php. ?>
-
-		<?php edit_post_link(); ?>
-
-	</article>
-	<!-- /article -->
-
-<?php endwhile; ?>
-
-<?php else : ?>
-
-	<!-- article -->
-	<article>
-		<h2><?php esc_html_e( 'Sorry, nothing to display.', 'html5blank' ); ?></h2>
-	</article>
-	<!-- /article -->
+<?php if (have_posts()): ?>
+	
+	<div class="row cards" style="margin-top: 3em;">
+ 	
+		<?php while (have_posts()) : the_post();
+	 	
+	 	$title = get_the_title();
+		$date = get_the_date();
+		$blurb = get_the_excerpt();
+		$newsletter = in_category('newsletter'); ?>
+	
+		<!-- article -->
+		<article id="post-<?php the_ID(); ?>" <?php post_class('col col-lg-4 col-md-4 col-sm-6 col-xs-12'); ?>>
+	
+			<!-- post thumbnail -->
+			<?php if ( has_post_thumbnail() ) : // Check if thumbnail exists. ?>
+				<a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>">
+					<?php the_post_thumbnail( array( 120, 120 ) ); // Declare pixel size you need inside the array. ?>
+				</a>
+			<?php endif; ?>
+			<!-- /post thumbnail -->
+	
+			<div class="col_inner">
+				<h3 style="margin-bottom: 0;"><?php echo $title ?></h3>
+				<p class="date"><?php echo $date ?></p>
+				<p class="excerpt"><?php echo $blurb ?></p>
+				<p class="inline">
+					<?php if ( $newsletter ) : ?>
+					<a class="read_more" href="<?php echo the_permalink(); ?>"><?php echo __('View newsletter'); ?></a>
+					<?php else : ?>
+					<a class="read_more" href="<?php echo the_permalink(); ?>"><?php echo __('Read post'); ?></a>
+					<?php endif; ?>
+				</p>
+			</div>
+	
+		</article>
+		<!-- /article -->
+	
+		<?php endwhile; ?>
+	
+		<?php else : ?>
+	
+		<!-- article -->
+		<article>
+			<h2><?php esc_html_e( 'Sorry, nothing to display.', 'html5blank' ); ?></h2>
+		</article>
+		<!-- /article -->
+	</div>
 
 <?php endif; ?>
