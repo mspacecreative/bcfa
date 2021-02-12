2855b706dfaba12b531aba3d70d98871604baba7
Matt <matt@mspacecreative.com>
2021-02-12 14:07:32 -0400

    c

--- sidebar.php
+++ sidebar.php
@@ -6,10 +6,6 @@
 			<?php if ( ! function_exists( 'dynamic_sidebar' ) || ! dynamic_sidebar( 'widget-area-1' ) ) ?>
 		</div>
 	
-		<div class="sidebar-widget">
-			<?php if ( ! function_exists( 'dynamic_sidebar' ) || ! dynamic_sidebar( 'widget-area-2' ) ) ?>
-		</div>
-	
 	</aside>
 </div>
 <!-- /sidebar -->
