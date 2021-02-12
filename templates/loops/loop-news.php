983bc53d6a0e78008e995c14c9f5a098696ef6be
Matt <matt@mspacecreative.com>
2021-02-12 13:15:32 -0400

    c

--- templates/loops/loop-news.php
+++ templates/loops/loop-news.php
@@ -5,33 +5,42 @@ $args = array(
 );
 	
 $loop = new WP_Query( $args );
-if ( $loop->have_posts() ) :
-while ( $loop->have_posts() ) : $loop->the_post();
+if ( $loop->have_posts() ) : ?>
 
-$title = get_the_title();
-$date = get_the_date();
-$blurb = get_the_excerpt(); ?>
+<div class="row cards">
 
-<div class="col col-lg-4 col-md-4 col-sm-6 col-xs-12">
-	<div class="col_inner">
-		<h3 style="margin-bottom: 0;"><?php echo $title ?></h3>
-		<p class="date"><?php echo $date ?></p>
-		<p class="excerpt"><?php echo $blurb ?></p>
-		<p class="inline">
-			<a class="read_more" href="<?php echo the_permalink(); ?>"><?php echo __('Read post'); ?></a>
-		</p>
+	<?php while ( $loop->have_posts() ) : $loop->the_post();
+	
+	$title = get_the_title();
+	$date = get_the_date();
+	$blurb = get_the_excerpt();
+	$newsletter = get_the_category($post->ID); ?>
+	
+	<div class="col col-lg-4 col-md-4 col-sm-6 col-xs-12">
+		<div class="col_inner">
+			<h3 style="margin-bottom: 0;"><?php echo $title ?></h3>
+			<p class="date"><?php echo $date ?></p>
+			<p class="excerpt"><?php echo $blurb ?></p>
+			<p class="inline">
+				<?php if ( $newsletter === 'newsletter' ) : ?>
+				<a class="read_more" href="<?php echo the_permalink(); ?>"><?php echo __('View newsletter'); ?></a>
+				<?php else : ?>
+				<a class="read_more" href="<?php echo the_permalink(); ?>"><?php echo __('Read post'); ?></a>
+				<?php endif; ?>
+			</p>
+		</div>
 	</div>
-</div>
-
-<?php 
-endwhile;
-
-else : ?>
-<div class="col col-lg-4 col-md-4 col-sm-6 col-xs-12">
-	<div class="col_inner">
-		<h2><?php echo __('News'); ?></h2>
-		<?php echo __('Sorry, no postings yet'); ?>
+	
+	<?php endwhile; ?>
+	
+	<?php else : ?>
+	<div class="col col-lg-12 col-md-12 col-sm-12 col-xs-12">
+		<div class="col_inner">
+			<h2><?php echo __('News'); ?></h2>
+			<?php echo __('Sorry, no postings yet'); ?>
+		</div>
 	</div>
+
 </div>
 
 <?php 
