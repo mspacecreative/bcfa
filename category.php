5191511665e3adc1083a7516f3363a4567c019a0
Matt <matt@mspacecreative.com>
2021-02-12 13:59:46 -0400

    c

--- category.php
+++ category.php
@@ -6,20 +6,10 @@
 			
 			<div class="innerContainer">
 				
-				<div class="row sidebar_layout">
-					
-					<div class="col col-lg-8 col-md-8 col-sm-12 col-xs-12 bottomMarginMobile">
-
-						<h1 class="page_title"><?php esc_html_e( 'Category: ', 'html5blank' ); single_cat_title(); ?></h1>
+				<h1 class="page_title"><?php esc_html_e( 'Category: ', 'html5blank' ); single_cat_title(); ?></h1>
 			
-						<?php get_template_part( 'loop' ); ?>
-						<?php get_template_part( 'pagination' ); ?>
-						
-					</div>
-					
-				</div>
-				
-				<?php get_sidebar(); ?>
+				<?php get_template_part( 'loop' ); ?>
+				<?php get_template_part( 'pagination' ); ?>
 				
 			</div>
 
