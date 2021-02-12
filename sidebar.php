0f87ccc0a3321e5b92578ed8f9f6cd333df2b29f
Matt <matt@mspacecreative.com>
2021-02-12 08:12:48 -0400

    c

--- sidebar.php
+++ sidebar.php
@@ -2,8 +2,6 @@
 <div class="col col-lg-3 col-md-3 col-sm-12 col-xs-12">
 	<aside class="sidebar" role="complementary">
 	
-		<?php get_template_part( 'searchform' ); ?>
-	
 		<div class="sidebar-widget">
 			<?php if ( ! function_exists( 'dynamic_sidebar' ) || ! dynamic_sidebar( 'widget-area-1' ) ) ?>
 		</div>
